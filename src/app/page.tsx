e942eb31614b746794ed1a3cdbc83a62af08add5
bayurexxa17 <bayurexxa17@gmail.com>
2026-09-08 17:24:51 +0700

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { getSessionUser } from "@/lib/auth";
-
-export const dynamic = "force-dynamic";
-
-export default async function Home() {
-  const user = await getSessionUser();
-  redirect(user ? "/dashboard" : "/login");
-}
